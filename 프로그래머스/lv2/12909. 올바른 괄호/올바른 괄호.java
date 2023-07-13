acb04eaba5fbb8c5caf0b9f41a4d47c8d141a1ae
PARK SUNG YOON <63085533+psung616@users.noreply.github.com>
2023-07-13 13:25:46 +0900

    [level 2] Title: 올바른 괄호, Time: 16.60 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()){
+            if(c == '('){
+                stack.push(c);
+            } else if(c == ')'){
+                if(stack.isEmpty()){
+                    answer = false;
+                }else{
+                    stack.pop();
+                }
+            }
+        }
+        
+        if(!stack.isEmpty()){
+            answer = false;   
+        }
+
+        return answer;
+    }
+}
